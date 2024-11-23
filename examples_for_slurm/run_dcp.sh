072a2c7fcbd61ecd6252ecbc88b3664fa3395c80
Rhiju Das <rhiju@stanford.edu>
2024-11-22 16:20:41 -0800

    run_dcp.sh and run_du.sh

--- examples_for_slurm/run_dcp.sh
+++ examples_for_slurm/run_dcp.sh
@@ -4,27 +4,18 @@
 #SBATCH --error=dcp.e%j
 #SBATCH --partition=biochem,owners
 #SBATCH --time=2:00:00
-#SBATCH -n 16
+#SBATCH -n 24
 #SBATCH -N 1
 #SBATCH --mem=16G
 
-#src_dir=/scratch/groups/rhiju/rhiju/Experiments/Eterna_RYOP_Pilot2022_experiments/DataAnalysis/Data/NovaSeq_2023-08-02_RH_VW_MutMap
-#target_dir=/scratch/users/rhiju/Experiments/Eterna_RYOP_Pilot2022_experiments/DataAnalysis/Data/NovaSeq_2023-08-02_RH_VW_MutMap
-
-#src_dir=/scratch/groups/rhiju/rhiju/Experiments/Eterna_RYOP_Pilot2022_experiments/DataAnalysis/Data/NovaSeq_2023-08-01_RH_DasBigLib0-15k
-#target_dir=NovaSeq_2023-08-01_RH_DasBigLib0-15k
-
-#src_dir=/scratch/groups/rhiju/rhiju/Experiments/Eterna_RYOP_Pilot2022_experiments/DataAnalysis/Data/NovaSeq_2023-07-28_RH_OpenKnot1
-#target_dir=NovaSeq_2023-07-28_RH_OpenKnot1
-
-src_dir=/scratch/groups/rhiju/rhiju/Experiments/Eterna_RYOP_Pilot2022_experiments/DataAnalysis/Data/NovaSeq_2023-06-06_RH_SL5_M2seq
-target_dir=NovaSeq_2023-06-06_RH_SL5_M2seq
+src_dir=Lane3_RPT
+target_dir=Lane3_cmuts
 
 echo "Copying ${src_dir} to ${target_dir}..."
 
 ml reset
 ml system mpifileutils
-srun -n 16 dcp -p ${src_dir} ${target_dir}
+srun -n 24 dcp -p ${src_dir} ${target_dir}
 # reset modules.
 source ~/.bash_profile
 
--- examples_for_slurm/run_du.sh
+++ examples_for_slurm/run_du.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=du
-#SBATCH --output=du.out
-#SBATCH --error=du.err
+#SBATCH --output=du.o%j
+#SBATCH --error=du.e%j
 #SBATCH --partition=biochem
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
 ##SBATCH --mail-type=ALL
 
-du -h -d 2
+du -h -d 1
 echo "DONE"
