b46903859f70e75f821df93bca2a3e8459d630c6
Rhiju Das <rhiju@stanford.edu>
2025-03-17 07:47:20 -0700

    -t option for slurm job time in hours

--- prepare_slurm.py
+++ prepare_slurm.py
@@ -13,11 +13,12 @@ parser.add_argument('commands_file',help='Text file with all commands in separat
 parser.add_argument('-j','--jobs_per_slurm_node', default=24,type=int,help='Number of jobs for each node' )
 parser.add_argument('-m','--memory_per_job', default=2,type=int,help='Number of Gb memory per job. Assume one core per job!')
 parser.add_argument('-n','--job_name', default='',help='Name of job for slurm')
+parser.add_argument('-t','--job_time', default=48,type=int,help='Number of hours to request per node')
 
 args = parser.parse_args()
 commands_file = args.commands_file
 job_name = args.job_name
-if len(job_name)==0: job_name = commands_file
+if len(job_name)==0: job_name = commands_file.replace('.sh','').replace('.txt','')
 
 assert( os.path.isfile( commands_file ) )
 command_lines = [x.strip()  for x in open( commands_file ).readlines() if len(x.strip())>0 and x.strip()[0] != '#' ]
@@ -38,8 +39,8 @@ for (i,command_line) in enumerate(command_lines):
     commands.append( command )
     if (i+1) % args.jobs_per_slurm_node == 0 or (i+1) == num_command_lines:
         njobs = len(commands)
-        sbatch_preface = '#!/bin/bash\n#SBATCH --job-name=%s\n#SBATCH --output=%s.o%%j\n#SBATCH --error=%s.e%%j\n#SBATCH --partition=biochem,owners\n#SBATCH --time=48:00:00\n#SBATCH -n %d\n#SBATCH -N 1\n#SBATCH --mem=%dG\n\n' \
-                 % (job_name,job_name,job_name,njobs,args.memory_per_job * njobs)
+        sbatch_preface = '#!/bin/bash\n#SBATCH --job-name=%s\n#SBATCH --output=%s.o%%j\n#SBATCH --error=%s.e%%j\n#SBATCH --partition=biochem,owners\n#SBATCH --time=%d:00:00\n#SBATCH -n %d\n#SBATCH -N 1\n#SBATCH --mem=%dG\n\n' \
+                 % (job_name,job_name,job_name,args.job_time,njobs,args.memory_per_job * njobs)
         fid_slurm.write( sbatch_preface )
         for command in commands: fid_slurm.write( command +'\n' )
         fid_slurm.write('\nwait\necho "DONE"\n')
