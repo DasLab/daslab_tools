2df3bef627d5579dd0dff1f17da9a578c473b6f3
Rhiju Das <rhiju@stanford.edu>
2025-12-22 15:15:05 -0800

    --tabular, and allow pdb input to be globbed

--- USalign.py
+++ USalign.py
@@ -7,16 +7,18 @@ from os.path import exists,basename
 from operator import add
 from math import sqrt
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(description='Run USalign on a bunch of PDBs.')
 parser.add_argument('refpdb', help='Reference PDB file')
-parser.add_argument('pdb', type=str, nargs='+', help='PDB file to align')
+parser.add_argument('pdb', type=str, nargs='+', help='PDB file(s) to align. Can also be a string in quotes to allow glob')
 parser.add_argument('-dump', action='store_true', help='Prepare superposition PDB as .TMsup.pdb ')
 parser.add_argument('-TMscore', type=int,default=0, help='integer setting for TMscore (0 by default means ignore sequence) ')
 parser.add_argument('-RNA', action='store_true', help='only align RNA chains')
 parser.add_argument('-force_mm', action='store_true', help='force align multimer (default auto-detect)')
 parser.add_argument('-force_monomer', action='store_true', help='force align monomer (default auto-detect)')
 parser.add_argument('-atom', default=" C3'", help='atom representative, 4 characters (default: " C3\'\") ')
+parser.add_argument('-t','--tabular', action='store_true', help='output in csv format')
 
 args = parser.parse_args()
 
@@ -30,19 +32,29 @@ mm = len(lines)>1
 if args.force_mm: mm = True
 if args.force_monomer: mm = False
 
-
 EXEC = 'USalign'
 
-for i in range(len(args.pdb)):
-    if not exists( args.pdb[i] ):
-        stderr.write( 'Could not find PDB file: '+args.pdb[i]+'\n' )
-        exit(0)
+pdbs = []
+for pdb in args.pdb:
+    if exists( pdb ):
+        pdbs.append( pdb )
+    else:
+        globfiles = glob.glob( pdb )
+        if len( globfiles ) == 0:
+            stderr.write( 'Could not find PDB file: '+pdb+'\n' )
+            exit(0)
+        pdbs.extend( sorted(globfiles) )
 
-    cmdline = '%s %s %s -TMscore %d -atom "%4s"' % (EXEC, args.pdb[i], args.refpdb, args.TMscore, args.atom)
+if args.tabular:
+    print( '%s,%s,%s' % ('ref','query','TMscore' ) )
+
+for i in range(len(pdbs)):
+
+    cmdline = '%s %s %s -TMscore %d -atom "%4s"' % (EXEC, pdbs[i], args.refpdb, args.TMscore, args.atom)
     if args.RNA: cmdline += ' -mol RNA'
     if mm:  cmdline += ' -mm 1 -ter 1'
     if args.dump:
-        sup_model_file = args.pdb[i].replace( '.pdb','' ) + '.TMsup.pdb'
+        sup_model_file = pdbs[i].replace( '.pdb','' ) + '.TMsup.pdb'
         cmdline += ' -o %s' % sup_model_file
     lines = popen( cmdline ).readlines()
 
@@ -56,6 +68,9 @@ for i in range(len(args.pdb)):
             nalign = int( line.split()[2].replace(',','') )
             rmsd_align = float( line.split()[4].replace(',',''))
 
-    print( '%s -vs- %s: %9.5f over %d residues   TM-score: %f' % (args.refpdb, args.pdb[i], rmsd_align, nalign, TMscore) )
+    if args.tabular:
+        print( '%s,%s,%f' % (args.refpdb, pdbs[i], TMscore) )
+    else:
+        print( '%s -vs- %s: %9.5f over %d residues   TM-score: %f' % (args.refpdb, pdbs[i], rmsd_align, nalign, TMscore) )
     #print 'TM-score: %f' % (TMscore)
 
