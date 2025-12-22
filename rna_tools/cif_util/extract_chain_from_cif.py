fefdd56742de3fc5d01ff90c73c76c260ff5adfa
Rhiju Das <rhiju@stanford.edu>
2025-12-22 11:44:40 -0800

    fix num_lines and add --C1prime_only option to extract_chain_from_cif.py

--- rna_tools/cif_util/extract_chain_from_cif.py
+++ rna_tools/cif_util/extract_chain_from_cif.py
@@ -4,7 +4,7 @@ import gzip
 import argparse
 from Bio import PDB
 
-def extract_chain_from_cif(cif_file, chain_id, output_file):
+def extract_chain_from_cif(cif_file, chain_id, output_file, C1prime_only = False):
     # Create a parser to read CIF files
     parser = PDB.MMCIFParser(QUIET=True)
 
@@ -26,16 +26,12 @@ def extract_chain_from_cif(cif_file, chain_id, output_file):
                     # Write the atoms of the specified chain to the PDB file
                     for residue in chain:
                         for atom in residue:
+                            if C1prime_only and not atom.name=="C1'": continue
                             # Format and write atom in PDB format
                             pdb_file.write(
-                                f'ATOM  {atom.serial_number:>5d}  {atom.name:4s}{residue.resname:>3s} {chain.id:1s}{residue.id[1]:>4d}{residue.id[2]:1s}   {atom.coord[0]:>8.3f}{atom.coord[1]:>8.3f}{atom.coord[2]:>8.3f}{atom.occupancy:>6.2f}{atom.bfactor:>6.2f}           {atom.element}\n'
+                                f'ATOM  {atom.serial_number:>5d}  {atom.name:4s}{residue.resname:>3s}{chain.id[-2:]:>2s}{residue.id[1]:>4d}{residue.id[2]:1s}   {atom.coord[0]:>8.3f}{atom.coord[1]:>8.3f}{atom.coord[2]:>8.3f}{atom.occupancy:>6.2f}{atom.bfactor:>6.2f}           {atom.element}\n'
                             )
-                            num_lines += 11
-                            #f"ATOM  {atom.serial_number:5} "
-                            #f"{atom.name:4} {residue.resname:3} "
-                            #f"{chain.id:1} {residue.id[1]:4} "
-                            #f"{atom.coord[0]:8.3f}{atom.coord[1]:8.3f}{atom.coord[2]:8.3f} "
-                            #f"{atom.occupancy:6.2f}{atom.bfactor:6.2f}          {atom.element:>2}\n"
+                            num_lines += 1
 
     print(f"Successfully extracted {num_lines} lines from chain {chain_id} to {output_file}")
 
@@ -45,6 +41,7 @@ def main():
     parser.add_argument('cif_file', help='Input CIF file (PDB.cif or PDB.cif.gz)')
     parser.add_argument('chain_id', help='Chain ID to extract')
     parser.add_argument('-o', '--output', default='', help='Output PDB file name (default: {PDB}_{chain}.pdb)')
+    parser.add_argument('--C1prime_only', action='store_true', help='Only output C1'' atoms')
 
     args = parser.parse_args()
 
@@ -52,7 +49,7 @@ def main():
     if not args.output:
         args.output = f"{args.cif_file.split('.')[0]}_{args.chain_id}.pdb"
 
-    extract_chain_from_cif(args.cif_file, args.chain_id, args.output)
+    extract_chain_from_cif(args.cif_file, args.chain_id, args.output, args.C1prime_only )
 
 if __name__ == '__main__':
     main()
